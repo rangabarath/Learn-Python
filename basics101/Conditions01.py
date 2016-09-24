0e985cc5f2b19a1d636d04d229166e8369f721a1
Ranganathan Barathan <ranganathanbarath@gmail.com>
2016-09-24 11:38:11 -0500

    Simple conditions

--- basics101/Conditions01.py
+++ basics101/Conditions01.py
@@ -1,7 +1,11 @@
+# Create condition variable and set value as TRUE
 cond = True
+# Check the variable condition
 if cond:
+    # Print 'True' if the condition is true
     print("True")
 else:
-  print("False")
+    # Print 'False' if the condition is false
+    print("False")
 
 
